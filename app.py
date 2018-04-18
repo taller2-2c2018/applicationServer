6b810b65d5b074562f337f3600a1492d615102f5
Fernando Cancellara <fdcancellara@gmail.com>
2018-04-18 02:18:16 -0300

    Prepparing for Heroku deploy

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
+import os
 from flask import Flask
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
 import pprint
+
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/applicationServerDB"
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = MONGO_URL
+mongoDB = PyMongo(app)
 
 mongoClient = MongoClient('localhost', 27017)
 dataBase = mongoClient.applicationServerDB
 table = dataBase.table
 
-app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     app.logger.info('Logging info before setting into database')
-    table.insert({"key":"value"})
+    mongoDB.db.collection.insert({"key":"PEPOTE"})
     return 'Inserted key value!'
 
 @app.route('/get')
 def getValues():
-    got_table = table.find({"key":"value"})
-    got_table_index_zero = got_table[0]
-    pprint.pprint(table.find_one())
+    got_table = mongoDB.db.collection.find({"key":"value"})
+    pprint.pprint(mongoDB.db.collection.find_one())
     table_string = str(got_table)
 
     app.logger.warn('%s', table_string)
-    return pprint.pformat(table.find_one())
+    return pprint.pformat(mongoDB.db.collection.find_one())
 
 
 
