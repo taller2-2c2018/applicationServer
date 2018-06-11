81d0645f4f6e2456a4efedd611988d88c2f41a81
Fernando Cancellara <fdcancellara@gmail.com>
2018-06-10 23:49:10 -0300

    Modifying travis coveralls configuration

--- travis-test.sh
+++ travis-test.sh
@@ -7,5 +7,5 @@ export MONGO_URL=mongodb://127.0.0.1:27017/applicationServerDB
 export REDIS_HOST=localhost
 export ANDROID_APP_TOKEN=appmock
 
-python -m pytest tests/app/* --cov-config .coveragerc --cov=$(pwd)
+python -m pytest tests/app/* --cov-config .coveragerc --cov=$(pwd)/appserver
 COVERALLS_REPO_TOKEN=tYDtwIUPZennl1BOFr1lf5JOV2lCM9Go2 coveralls
