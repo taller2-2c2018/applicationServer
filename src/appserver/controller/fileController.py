aca1add603bb179f7e78efe56661966d5ed3e093
Fernando Cancellara <fdcancellara@gmail.com>
2018-06-22 00:19:18 -0300

    Adding try catch to external services

--- src/appserver/controller/fileController.py
+++ src/appserver/controller/fileController.py
@@ -1,4 +1,4 @@
-from flask import request, Blueprint
+from flask import Blueprint
 
 from appserver.logger import LoggerFactory
 from appserver.monitor.monitor import monitor
--- src/appserver/datastructure/ApplicationResponse.py
+++ src/appserver/datastructure/ApplicationResponse.py
@@ -6,6 +6,7 @@ CREATED = 201
 BAD_REQUEST = 400
 NOT_FOUND = 404
 UNAUTHORIZED = 401
+SERVICE_UNAVAILABLE = 503
 
 
 class ApplicationResponse:
@@ -26,15 +27,19 @@ class ApplicationResponse:
         return ApplicationResponse.__json_response(BAD_REQUEST, message, None)
 
     @staticmethod
-    def file(file):
-        return Response(file, mimetype='application/octet-stream')
+    def service_unavailable(message=''):
+        return ApplicationResponse.__json_response(SERVICE_UNAVAILABLE, message, None)
 
     @staticmethod
-    def __create_json(status, message="", data=None):
-        response = {'status': status, 'message': message, 'data': data}
-        return json.dumps(response, default=str)
+    def file(file):
+        return Response(file, mimetype='application/octet-stream')
 
     @staticmethod
     def __json_response(status, message, data):
         message_json = ApplicationResponse.__create_json(status, message, data)
         return Response(message_json, status=status, mimetype='application/json')
+
+    @staticmethod
+    def __create_json(status, message="", data=None):
+        response = {'status': status, 'message': message, 'data': data}
+        return json.dumps(response, default=str)
