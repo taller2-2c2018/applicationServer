f7c1d29d30f4fae4c3cb39148a4344ccba1deaf2
Fernando Cancellara <fdcancellara@gmail.com>
2018-07-10 23:22:18 -0300

    Adding thread to monitor

--- src/appserver/monitor/monitor.py
+++ src/appserver/monitor/monitor.py
@@ -7,6 +7,7 @@ from flask import Blueprint, request, jsonify
 from functools import wraps
 
 from appserver import app
+from appserver.threading.RunAsync import run_async
 from appserver.time.Time import Time
 
 monitor_controller = Blueprint('monitor', __name__)
@@ -16,22 +17,27 @@ monitor_collection = app.database.monitor
 def monitor(method):
     @wraps(method)
     def decorated_function(*args, **kwargs):
-        ts = time.time()
+        time_start = time.time()
         result = method(*args, **kwargs)
-        te = time.time()
+        time_end = time.time()
 
-        request_data = {
-            'route': request.path,
-            'method': request.method,
-            'date_time': Time.now(),
-            'day': time.strftime('%Y-%m-%d'),
-            'hour': time.strftime('%H'),
-            'time_elapsed_ms': int((te - ts) * 1000)
-        }
-        monitor_collection.insert_one(request_data)
+        monitor_data(request.path, request.method, time_end, time_start)
         return result
 
     return decorated_function
+
+
+@run_async
+def monitor_data(path, method, time_end, time_start):
+    request_data = {
+        'route': path,
+        'method': method,
+        'date_time': Time.now(),
+        'day': time.strftime('%Y-%m-%d'),
+        'hour': time.strftime('%H'),
+        'time_elapsed_ms': int((time_end - time_start) * 1000)
+    }
+    monitor_collection.insert_one(request_data)
 
 
 @monitor_controller.route('/monitor/')
@@ -50,11 +56,11 @@ def monitor_route():
     aware_colection = monitor_collection.with_options(
         codec_options=CodecOptions(tz_aware=True, tzinfo=pytz.timezone('America/Argentina/Buenos_Aires')))
     cursor = aware_colection.aggregate(pipeline)
-    data =  {}
+    data = {}
     for row in cursor:
-        print(row, file = sys.stderr)
+        print(row, file=sys.stderr)
         request_string = row['_id']['method'] + row['_id']['route']
-        if not request_string in data:
+        if request_string not in data:
             data[request_string] = []
 
         data[request_string].append({
